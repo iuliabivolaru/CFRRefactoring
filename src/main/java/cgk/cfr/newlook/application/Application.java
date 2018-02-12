a0cd68db8898bc4dc87b4d9274587fe67c1aade4
Iulia Bivolaru <ibivolar@its.jnj.com>
2018-02-12 15:16:58 +0200

    create project skeleton

---
+++ src/main/java/cgk/cfr/newlook/application/Application.java
@@ -0,0 +1,15 @@
+package cgk.cfr.newlook.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by iuliab on 09.02.2018.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
